e4a0e79bcaf35088ca4efebad7281da1fd76b94f
Alexey Gorbunov <a.gorbunov4221@gmail.com>
2024-11-17 21:31:59 +0700

    removed hard-coded name

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import asyncio
 from service.server import websocket_endpoint
 from fastapi import FastAPI, WebSocket
+from settings.config import MAIN_SERVER_NAME
 import sys
 import os
 # Добавляем путь проекта
@@ -9,7 +10,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 app = FastAPI()
 
-@app.websocket("/response")
+@app.websocket(f"/{MAIN_SERVER_NAME}")
 async def response_ws(websocket: WebSocket):
     await websocket_endpoint(websocket)
 
